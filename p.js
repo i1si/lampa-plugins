f3573869e99d70ec426cae743d46a3377bbac082
i1si <ivan_sidorchuk@icloud.com>
2024-10-01 20:11:08 +0300

    f

--- p.js
+++ p.js
@@ -1,5 +1,13 @@
 (function () {
     'use strict';
 
-    alert('ok');
+    screen = new Proxy(screen, {
+        apply: function (target, thisArg, argumentsList) {
+          if (argumentsList[0] == 'tv') {
+            return true
+          }else{
+            return target(argumentsList[0])
+          }
+        }
+      })
 })();
