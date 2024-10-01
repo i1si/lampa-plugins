3640e21c1fa1a662bf9b30f87633bb30cc3eb75d
i1si <ivan_sidorchuk@icloud.com>
2024-10-01 09:55:49 +0300

    test

--- p.js
+++ p.js
@@ -1,24 +1,12 @@
 (function () {
     'use strict';
-    var screen = function (need) {
-        if (need == 'light') {
-            return Storage.field('light_version') && screen('tv');
-        }
-        if (need == 'tv') {
+    screen = new Proxy(screen, {
+        get(target, arr){
+          if (arr == 'tv') {
             return true
+          }else{
+            return target(arr)
+          }
         }
-
-        var is_tv = true;
-
-        if (true) {
-            var ratio = 0.8;
-            var width = 0;
-            var height = 0;
-            is_tv = width > height && width >= 1280;
-        }
-
-        if (need == 'tv') return is_tv;
-        if (need == 'mobile') return !is_tv;
-        return false;
-    }
+      })
 })();
